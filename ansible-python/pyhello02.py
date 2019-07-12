56719f77c2b9314aabca07f30c0cf675cb642461
Steve Petersen <steven.petersen@verizon.net>
2019-07-12 09:33:16 -0400

    anspy2

---
+++ ansible-python/pyhello02.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+print ("Hello, again my friend! How are you?")
+
+open("/home/student/mycode/ansible-python/stopper.txt", 'a').close() # this line ensures stopper.txt is created
